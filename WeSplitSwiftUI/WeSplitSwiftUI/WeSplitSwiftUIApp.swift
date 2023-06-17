a8717ff9d0238057d4fd4e38eda88c6c55c527ca
Mateusz Brychczynski <mat.brych@gmail.com>
2023-06-17 10:50:57 +0100

    Add files via upload

---
+++ WeSplitSwiftUI/WeSplitSwiftUI/WeSplitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitSwiftUIApp.swift
+//  WeSplitSwiftUI
+//
+//  Created by Mateusz Brychczynski on 16/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
