145e52f9f2bd7ac0fd8249f221d4d58914b41cd7
Mateusz Brychczynski <mat.brych@gmail.com>
2023-06-22 14:28:27 +0100

    Add files via upload

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Mateusz Brychczynski on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
