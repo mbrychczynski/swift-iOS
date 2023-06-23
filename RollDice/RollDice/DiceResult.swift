6e20eee45386e3f9b229a1193c127c542f32fbd7
Mateusz Brychczynski <mat.brych@gmail.com>
2023-06-23 10:42:25 +0100

    Add files via upload

---
+++ RollDice/RollDice/DiceResult.swift
@@ -0,0 +1,30 @@
+//
+//  DiceResult.swift
+//  RollDice
+//
+//  Created by Mateusz Brychczynski on 23/06/2023.
+//
+
+import Foundation
+
+
+struct DiceResult: Identifiable, Codable {
+    var id = UUID()
+    var type: Int
+    var number: Int
+    var rolls = [Int]()
+    
+    var description: String {
+        rolls.map(String.init).joined(separator: ", ")
+    }
+    
+    init(type: Int, number: Int) {
+        self.type = type
+        self.number = number
+        
+        for _ in 0..<number {
+            let roll = Int.random(in: 1...type)
+            rolls.append(roll)
+        }
+    }
+}
---
+++ RollDice/RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Mateusz Brychczynski on 22/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
