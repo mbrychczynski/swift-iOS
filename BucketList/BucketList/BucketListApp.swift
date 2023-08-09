e259c5a4c82a49077de00b55fc360c36a58a2223
Mateusz Brychczynski <mat.brych@gmail.com>
2023-08-09 16:02:06 +0100

    Add files via upload

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Mateusz Brychczynski on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
