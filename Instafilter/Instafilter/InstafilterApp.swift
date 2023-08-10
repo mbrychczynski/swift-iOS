68b074d4ad839f09d7bfba5bb6c5fbb9749d8719
Mateusz Brychczynski <mat.brych@gmail.com>
2023-08-10 12:41:02 +0100

    Add files via upload

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Mateusz Brychczynski on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
